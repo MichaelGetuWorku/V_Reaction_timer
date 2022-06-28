e3bc59856554ab96e99f55a7885a23ae6702bc7f
MichaelGetuWorku <michael.getu.hh3221@gmail.com>
2022-06-28 17:45:51 +0300

    intial setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Reaction Timer</h1>
+</template>
+
+
+<script >
+export default {
+
+}
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+  color: #444;
+  text-align: center;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+    Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
